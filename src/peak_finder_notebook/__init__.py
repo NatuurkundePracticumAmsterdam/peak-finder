78e311372f2f010ece42b112435c7f1e7c530215
David Fokkema <davidfokkema@icloud.com>
2026-03-23 12:10:44 +0100

    Copy notebook into cwd

--- src/peak_finder_notebook/__init__.py
+++ src/peak_finder_notebook/__init__.py
@@ -1,7 +1,16 @@
+import pathlib
 import subprocess
 from importlib.resources import files
 
 
+def copy_from_package_if_missing(resource: str) -> None:
+    dest = pathlib.Path.cwd() / resource
+    if not dest.exists():
+        source = files("peak_finder_notebook").joinpath(resource)
+        dest.write_bytes(source.read_bytes())
+
+
 def main():
-    notebook = files("peak_finder_notebook").joinpath("peak-finder.ipynb")
-    subprocess.run(["jupyter", "lab", str(notebook)], check=True)
+    copy_from_package_if_missing("peak-finder.ipynb")
+    copy_from_package_if_missing("example-datafile.csv")
+    subprocess.run(["jupyter", "lab", "peak-finder.ipynb"], check=True)
